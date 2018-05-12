5efcab29b15f1f00937ab4b8138155c09baec556
AyaaaAhmed <38727600+AyaaaAhmed@users.noreply.github.com>
2018-05-12 23:35:54 +0200

    Add files via upload

---
+++ src/main/java/Repository/collaboratorRepository.java
@@ -0,0 +1,9 @@
+package Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import Entity.Collaborator;
+
+public interface collaboratorRepository extends CrudRepository<Collaborator, Integer>  {
+
+}
